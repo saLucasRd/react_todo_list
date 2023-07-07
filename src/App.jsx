cbec47441ff66656f703a763bb6f7901e4f7301b
Lucas Rodrigues <sa.lucas.rd@gmail.com>
2023-07-07 10:20:30 -0300

    third commit, check box wont work, delete is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,78 @@
-import './styles.css';
+import { useState } from "react"
+import "./styles.css"
 
 export default function App() {
-    return (
-        //note: use fragments to nest more than 1 element in a return
-        <>
-            <form className="new-item-form">
-                <div className="form-row">
-                    <label htmlFor="item">New Item</label>
-                    <input type="text" id="item" />
-                </div>
-                <button className="btn">Add</button>
-            </form>
-            <h1 className="header">Todo List</h1>
-            <ul className="list">
-                <li>
-                    <label>
-                        <input type="checkbox" />
-                        Item 1
-                    </label>
-                    <button className="btn btn-danger">Delete</button>
-                </li>
-                <li>
-                    <label>
-                        <input type="checkbox" />
-                        Item 2
-                    </label>
-                    <button className="btn btn-danger">Delete</button>
-                </li>
-            </ul>
-        </>
-    );
+   const [newItem, setNewItem] = useState("")
+   const [todos, setTodos] = useState([])
+
+   function handleSubmit(e) {
+      e.preventDefault()
+
+      setTodos(currentTodos => {
+         return [
+            ...currentTodos,
+            { id: crypto.randomUUID(), title: newItem, completed: false },
+         ]
+      })
+
+      setNewItem("")
+   }
+
+   function toggleTodo(id, completed) {
+      setTodos(currentTodos => {
+         return currentTodos.map(todo => {
+            if (todo.id === id) {
+               return { ...todo, completed }
+            }
+
+            return todo
+         })
+      })
+   }
+
+   function deleteTodo(id) {
+      setTodos(currentTodos => {
+         return currentTodos.filter(todos => todos.id !== id)
+      })
+   }
+
+   return (
+      //note: use fragments to nest more than 1 element in a return
+      <>
+         <form onSubmit={handleSubmit} className="new-item-form">
+            <div className="form-row">
+               <label htmlFor="item">New Item</label>
+               <input
+                  value={newItem}
+                  onChange={e => setNewItem(e.target.value)}
+                  type="text"
+                  id="item"
+               />
+            </div>
+            <button className="btn">Add</button>
+         </form>
+         <h1 className="header">Todo List</h1>
+         <ul className="list">
+            {todos.map(todo => {
+               return (
+                  <li key={todo.id}>
+                     <label>
+                        <input
+                           type="checkbox"
+                           checked={todo.completed}
+                           onChange={e => toggleTodo(todo.id, e.target.checked)}
+                        />
+                        {todo.title}
+                     </label>
+                     <button
+                        onClick={() => deleteTodo(todo.id)}
+                        className="btn btn-danger">
+                        Delete
+                     </button>
+                  </li>
+               )
+            })}
+         </ul>
+      </>
+   )
 }

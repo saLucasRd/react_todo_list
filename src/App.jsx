791a9ac56ce17c46a9c58b7517781ccae687ae1b
Lucas Rodrigues <sa.lucas.rd@gmail.com>
2023-07-07 11:12:18 -0300

    4th commit everything went wrong lol, quiting project for now

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
-   const [newItem, setNewItem] = useState("")
    const [todos, setTodos] = useState([])
 
-   function handleSubmit(e) {
-      e.preventDefault()
-
+   function addTodo(title) {
       setTodos(currentTodos => {
          return [
             ...currentTodos,
-            { id: crypto.randomUUID(), title: newItem, completed: false },
+            { id: crypto.randomUUID(), title, completed: false },
          ]
       })
-
-      setNewItem("")
    }
 
    function toggleTodo(id, completed) {
@@ -39,20 +34,10 @@ export default function App() {
    return (
       //note: use fragments to nest more than 1 element in a return
       <>
-         <form onSubmit={handleSubmit} className="new-item-form">
-            <div className="form-row">
-               <label htmlFor="item">New Item</label>
-               <input
-                  value={newItem}
-                  onChange={e => setNewItem(e.target.value)}
-                  type="text"
-                  id="item"
-               />
-            </div>
-            <button className="btn">Add</button>
-         </form>
+         <NewTodoForm onSubmit={addTodo} />
          <h1 className="header">Todo List</h1>
          <ul className="list">
+            {todos.length === 0 && "No Todos"}
             {todos.map(todo => {
                return (
                   <li key={todo.id}>
